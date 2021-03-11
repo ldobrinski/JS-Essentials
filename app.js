7625c90fb757125e660c2ccd9ca6e21b7cc2f816
Lindsey Dobrinski <63567322+ldobrinski@users.noreply.github.com>
2021-03-11 13:40:36 -0800

    objects dot notation

---
+++ app.js
@@ -0,0 +1,19 @@
+const bag = {
+    name: "Everyday bag",
+    height: 12,
+    color: "purple",
+   weight: 5,
+   pocketNum: 4,
+   strapLength: {
+       left: 26,
+       right: 26,
+   }
+
+};
+
+console.log("The bag object:", bag);
+
+console.log("The pocketNum value:", bag.pocketNum);
+console.log("The color value", bag.color);
+console.log("The strapLength left", bag.strapLength.left);
+
